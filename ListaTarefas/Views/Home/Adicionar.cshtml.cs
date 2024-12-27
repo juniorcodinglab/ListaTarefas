0626f834b4ea2f8e354c99d8336f3435340a91d4
cobmais <silvio_cbsj@hotmail.com>
2024-12-27 16:37:37 -0300

    Adicionar arquivos de projeto.

---
+++ ListaTarefas/Views/Home/Adicionar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ListaTarefas.Views.Home
+{
+    public class AdicionarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
